0f57b7b3e8b74f9504bc1a4f75bd4e35adc47ebe
jeffoxenberg <jeff.oxenberg@hpe.com>
2022-11-05 11:52:17 -0400

    add explainability to UI

--- docker/api-server/main.py
+++ docker/api-server/main.py
@@ -1,10 +1,11 @@
 # conda install -c conda-forge fastapi uvicorn
 # uvicorn main:app --reload
 # https://fastapi.tiangolo.com/tutorial/body/
-
+import pandas as pd
 from fastapi import BackgroundTasks, FastAPI
 import os
 from pydantic import BaseModel
+import shap
 from sqlalchemy import create_engine
 from url_converter import build_inference
 import xgboost as xgb
@@ -28,6 +29,8 @@ def insert_into_db(df):
 app = FastAPI()
 
 clf = load_model()
+exp = shap.TreeExplainer(clf)
+
 engine = create_engine("postgresql://daan:" + os.environ['PG_PW'] +"@pg:5432/phishing_data")
 
 
@@ -48,7 +51,26 @@ async def predict(url: URL, background_tasks: BackgroundTasks):
                                   'qty_hashtag_domain',
                                   'qty_dollar_domain',
                                   'qty_percent_domain'], axis=1))
+
+    shap_values = exp.shap_values([d.drop(['qty_slash_domain',
+                                           'qty_questionmark_domain',
+                                           'qty_equal_domain',
+                                           'qty_and_domain',
+                                           'qty_exclamation_domain',
+                                           'qty_space_domain',
+                                           'qty_tilde_domain',
+                                           'qty_comma_domain',
+                                           'qty_plus_domain',
+                                           'qty_asterisk_domain',
+                                           'qty_hashtag_domain',
+                                           'qty_dollar_domain',
+                                           'qty_percent_domain'], axis=1).squeeze()])
+
     d['full_url'] = u
     d['phish_probability'] = round(i.tolist()[0][1], 3)
     background_tasks.add_task(insert_into_db, d)
-    return {'benign': i.tolist()[0][0], 'phishing': i.tolist()[0][1]}
+
+    return {'benign': i.tolist()[0][0], 'phishing': i.tolist()[0][1],
+            'shap': {'expected_value': float(exp.expected_value), 'shap_values': shap_values[0].tolist()},
+            'data': d.squeeze().to_json()}
+
--- docker/streamlit/Inference.py
+++ docker/streamlit/Inference.py
@@ -1,10 +1,18 @@
+import numpy as np
+import pandas as pd
+import shap
 import streamlit as st
+import streamlit.components.v1 as components
 import requests
+
+
+def st_shap(plot, height=None):
+    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+    components.html(shap_html, height=height)
 
 
 st.set_page_config(page_title="Inference")
 st.title('URLAnalysis: Phishing Detection Inference')
-
 with st.form('url_form'):
     url = st.text_input('URL', value='Enter URL')
     button = st.form_submit_button('Submit')
@@ -19,5 +27,23 @@ with st.form('url_form'):
             st.success('URL appears safe')
         st.write("Benign Probability: " + str(round(result['benign'], 3)))
         st.write("Phishing Probability: " + str(round(result['phishing'], 3)))
+        d = pd.Series(eval(result['data']))
+        st_shap(shap.force_plot(result['shap']['expected_value'],
+                                np.array(list(result['shap']['shap_values'])),
+                                d.drop(['qty_slash_domain',
+                                        'qty_questionmark_domain',
+                                        'qty_equal_domain',
+                                        'qty_and_domain',
+                                        'qty_exclamation_domain',
+                                        'qty_space_domain',
+                                        'qty_tilde_domain',
+                                        'qty_comma_domain',
+                                        'qty_plus_domain',
+                                        'qty_asterisk_domain',
+                                        'qty_hashtag_domain',
+                                        'qty_dollar_domain',
+                                        'qty_percent_domain',
+                                        'full_url',
+                                        'phish_probability']), link='logit'))
 
 st.markdown("**Note: URLs saved to database for further analysis**")
